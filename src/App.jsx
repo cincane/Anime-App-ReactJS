99e7686dc9afeea892aff884c0b3f8d998f8f94f
cincane <adhani181@gmail.com>
2025-12-19 00:31:23 +0700

    mengubah menjadi h3

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function Logo() {
     <div className="logo">
       <span role="img">🍥</span>
       <h1>WeeBoo</h1>
-      <h1>Halo</h1>
+      <h3>Haii</h3>
       <span role="img">🍥</span>
     </div>
   );
